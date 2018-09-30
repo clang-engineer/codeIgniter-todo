4ffbef070684a9598bccb512f6d13e324294045e
yorez <yroez@naver.com>
2018-09-30 20:03:38 +0900

    주소 줄이기(index.php생략) & delete 후 index초기화

--- application/models/Todo_m.php
+++ application/models/Todo_m.php
@@ -28,12 +28,23 @@ class Todo_m extends CI_Model
     {
         $sql="INSERT INTO items (content,created_on,due_date) VALUES ('".$content."', '".$created_on."', '".$due_date."')";
         $query=$this->db->query($sql);
-
     }
 
-    function delete_todo($id)
+    public function delete_todo($id)
     {
-      $sql="DELETE FROM items WHERE id='".$id."'";
-      $query=$this->db->query($sql);
+        $sql="DELETE FROM items WHERE id='".$id."'";
+        $query=$this->db->query($sql);
+
+        // ALTER TABLE [테이블 명] AUTO_INCREMENT=1;
+        // SET @COUNT = 0;
+        // UPDATE [테이블 명] SET [AUTO_INCREMENT 열 이름] = @COUNT:=@COUNT+1;
+
+        $sql0="ALTER TABLE items AUTO_INCREMENT=1";
+        $sql1="SET @COUNT = 0";
+        $sql2="UPDATE items SET id = @COUNT:=@COUNT+1";
+
+        $query=$this->db->query($sql0);
+        $query=$this->db->query($sql1);
+        $query=$this->db->query($sql2);
     }
 }
